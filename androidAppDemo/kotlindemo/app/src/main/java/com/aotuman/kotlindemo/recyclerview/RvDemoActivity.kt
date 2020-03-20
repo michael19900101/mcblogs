6a4b5c137997cb725d16a68560d20292fa6f26fa
aotuman <382968311@qq.com>
2020-03-20 23:29:56 +0800

    recyclerview demo first commit

---
+++ androidAppDemo/kotlindemo/app/src/main/java/com/aotuman/kotlindemo/recyclerview/RvDemoActivity.kt
@@ -0,0 +1,35 @@
+package com.aotuman.kotlindemo.recyclerview
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.aotuman.kotlindemo.R
+
+class RvDemoActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rvdemo_main)
+
+        val myDataset: Array<String> =  Array(30) { i -> "姓名：$i" }
+        viewManager = LinearLayoutManager(this)
+        viewAdapter = MyAdapter(myDataset)
+
+        recyclerView = findViewById<RecyclerView>(R.id.rvdemo_recyclerview).apply {
+            // use this setting to improve performance if you know that changes
+            // in content do not change the layout size of the RecyclerView
+            setHasFixedSize(true)
+
+            // use a linear layout manager
+            layoutManager = viewManager
+
+            // specify an viewAdapter (see also next example)
+            adapter = viewAdapter
+
+        }
+    }
+}
