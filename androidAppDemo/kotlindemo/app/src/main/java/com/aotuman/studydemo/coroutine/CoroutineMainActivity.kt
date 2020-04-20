e5615dc2199319de65ba1bdc4fba5e0e3e5907c3
huangkunting <382968311@qq.com>
2020-04-20 14:17:55 +0800

    测试内存泄露

---
+++ androidAppDemo/kotlindemo/app/src/main/java/com/aotuman/studydemo/coroutine/CoroutineMainActivity.kt
@@ -0,0 +1,13 @@
+package com.aotuman.studydemo.coroutine
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.aotuman.studydemo.R
+
+class CoroutineMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
