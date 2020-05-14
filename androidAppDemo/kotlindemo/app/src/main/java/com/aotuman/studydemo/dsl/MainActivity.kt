cddfab81d5f8102aabb078ea5bd799310993fc46
huangkunting <382968311@qq.com>
2020-05-14 11:13:50 +0800

    使用DSL 动态构建Android布局

---
+++ androidAppDemo/kotlindemo/app/src/main/java/com/aotuman/studydemo/dsl/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.aotuman.studydemo.dsl
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.aotuman.studydemo.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_dsl)
+    }
+}
