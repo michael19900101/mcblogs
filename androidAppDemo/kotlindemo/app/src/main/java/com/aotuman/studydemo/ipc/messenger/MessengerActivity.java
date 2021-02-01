666a8867a5ddf282df824d5b5be53e7e1683020b
aotuman <382968311@qq.com>
2021-02-01 22:23:54 +0800

    增加ipc demo

---
+++ androidAppDemo/kotlindemo/app/src/main/java/com/aotuman/studydemo/ipc/messenger/MessengerActivity.java
@@ -0,0 +1,71 @@
+package com.aotuman.studydemo.ipc.messenger;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.aotuman.studydemo.R;
+
+public class MessengerActivity extends AppCompatActivity {
+    private Messenger mMessenger;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent intent = new Intent(MessengerActivity.this, MessengerService.class);
+        bindService(intent, mServiceConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MessengerService.MSG_FROMCLIENT:
+                    Log.i(MessengerService.TAG, "客户端收到的信息-------" + msg.getData().get("rep"));
+                    break;
+            }
+        }
+    };
+
+
+    private ServiceConnection mServiceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            mMessenger = new Messenger(service);
+            Message mMessage = Message.obtain(null, MessengerService.MSG_FROMCLIENT);
+            Bundle mBundle = new Bundle();
+            mBundle.putString("msg", "这里是客户端，收到了嘛服务端");
+            mMessage.setData(mBundle);
+            //将Messenger传递给服务端
+            mMessage.replyTo = new Messenger(mHandler);
+            try {
+                mMessenger.send(mMessage);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbindService(mServiceConnection);
+
+    }
+}
