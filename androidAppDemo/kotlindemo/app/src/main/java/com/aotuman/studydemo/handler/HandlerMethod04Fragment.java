1dc79ea936476c6e328cab341ff7e154e7fb1fd4
HuangKunTing <382968311@qq.com>
2021-04-04 17:08:36 +0800

    更新子线程创建Handler的另外一种方法，但是子线程不能更新UI,而且页面销毁的时候，要调用looper.quit()去停止死循环，保证不会内存泄露，资源回收及时

--- androidAppDemo/kotlindemo/app/src/main/java/com/aotuman/studydemo/handler/HandlerMethod04Fragment.java
+++ androidAppDemo/kotlindemo/app/src/main/java/com/aotuman/studydemo/handler/HandlerMethod04Fragment.java
@@ -1,5 +1,7 @@
 package com.aotuman.studydemo.handler;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -10,12 +12,15 @@ import androidx.navigation.Navigation;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.aotuman.studydemo.R;
+
+import java.lang.ref.WeakReference;
 
 /**
  * 子线程创建Handler
@@ -82,5 +87,54 @@ public class HandlerMethod04Fragment extends Fragment {
         // 步骤5：开启工作线程（同时启动了Handler）
 
 
+
+
+    }
+
+    void test() {
+        new Thread() {
+            @Override
+            public void run() {
+                Looper.prepare();
+                // 步骤1：在子线程中 通过匿名内部类 创建Handler类对象
+                mHandler = new Handler(){
+                    // 通过复写handlerMessage()从而确定更新UI的操作
+                    @Override
+                    public void handleMessage(Message msg) {
+                        // 根据不同线程发送过来的消息，执行不同的UI操作
+                        switch (msg.what) {
+                            case 1:
+                                Log.d("jjjj","收到消息");
+                                break;
+                        }
+                    }
+                };
+                Looper.loop();
+
+                Log.d("jjjj","测试程序能否到达这里");
+
+            }
+        }.start();
+
+        new Thread(()->{
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            // 步骤3：创建所需的消息对象
+            Message msg = Message.obtain();
+            msg.what = 1; // 消息标识
+            msg.obj = "A"; // 消息内存存放
+
+            // 步骤4：在工作线程中 通过Handler发送消息到消息队列中
+            mHandler.sendMessage(msg);
+        }).start();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+//        mHandler.getLooper().quit();
     }
 }
